0a765a927aa7f17e43b1461c790f766bb694eab1
pandeyakash <pandeyakash0502@gmail.com>
2024-07-31 23:57:14 +0530

    Added Comments

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
+//Selected the container in which the images will be appended on scroll
 const container = document.querySelector(".container");
 
+//This variable represents the current page number used in the url as query parameter
 let page = 1;
+
+//Used a flag variable for not making unnecessary fetch call until the data is fully loaded
 let flag = false;
 
+//Called getData function to get 1st page data.
 getData(`https://jsonplaceholder.typicode.com/photos?_page=${page}&_limit=8`);
 
+//Added event listener on window object for scroll. And will call the get data function when the scroll bar reaches its end.
 window.addEventListener("scroll", () => {
   const { scrollTop, clientHeight, scrollHeight } = document.documentElement;
   console.log(scrollTop, clientHeight, scrollHeight);
@@ -18,11 +24,16 @@ window.addEventListener("scroll", () => {
   }
 });
 
+//Async funtion for fetching the data and calling dislayData() function for appending data to the container
 async function getData(url) {
-  const res = await fetch(url);
-  const data = await res.json();
-  console.log(data);
-  displayData(data);
+  try {
+    const res = await fetch(url);
+    const data = await res.json();
+    console.log(data);
+    displayData(data);
+  } catch (error) {
+    console.log(error);
+  }
 }
 
 function displayData(data) {
